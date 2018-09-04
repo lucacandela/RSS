5be9907aea27c938634382fefc7a07c68fb8477d
lucacandela <39223795+lucacandela@users.noreply.github.com>
2018-09-03 20:56:53 -0700

    Add files via upload

---
+++ config2.py
@@ -0,0 +1,8 @@
+username = "reddit username"
+password = "reddit password"
+client_id = "reddit client_id"
+client_secret = "reddit client_secret"
+
+account_sid = "twilio account_sid"
+auth_token = "twilio auth_token"
+checkedsubmissions = []
